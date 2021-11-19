7ba6185abd36fa1576a4df819c8fb541aca84dfe
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-11-19 11:45:22 -0800

    all issues resolved

--- src/screens/authenticationScreen/loginScreen/loginScreen.js
+++ src/screens/authenticationScreen/loginScreen/loginScreen.js
@@ -1,20 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import {View, ScrollView} from 'react-native';
-import {useFormik} from 'formik';
-import {useToast} from 'native-base';
-import {loginValidationSchema} from '../../../shared/validation/authValidation';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFormik } from 'formik';
+import { useToast } from 'native-base';
+import { loginValidationSchema } from '../../../shared/validation/authValidation';
 import style from './loginScreenStyle';
 import InputFieldsHeader from '../../../components/common/inputFieldsHeader/inputFieldsHeader';
 import InputField from '../../../components/common/inputField/inputField';
 import CustomButton from '../../../components/common/button/button';
 import NavigationLink from '../../../components/common/navigationLink/navigationLink';
-import {login} from '../../../shared/services/authServices';
+import { login } from '../../../shared/services/authServices';
 import CustomToast from '../../../components/common/customToast/customToast';
-import {getVolunteerById} from '../../../../graphql/queries';
-import {useLazyQuery} from '@apollo/client';
+import { getVolunteerById } from '../../../../graphql/queries';
+import { useLazyQuery } from '@apollo/client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function LoginScreen({navigation}) {
+export default function LoginScreen({ navigation }) {
   let [isLoading, setIsLoading] = useState(false);
   let [showInvalidInput, setShowInvalidInput] = useState(false);
   let [getVolunteer, volunteerData] = useLazyQuery(getVolunteerById);
@@ -31,18 +31,18 @@ export default function LoginScreen({navigation}) {
           setIsLoading(false);
           navigation.reset({
             index: 0,
-            routes: [{name: 'drawer'}],
+            routes: [{ name: 'drawer' }],
           });
         })
         .catch(err => {
           setIsLoading(false);
           navigation.reset({
             index: 0,
-            routes: [{name: 'drawer'}],
+            routes: [{ name: 'drawer' }],
           });
         });
     }
-  }, [volunteerData?.data?.getVolunteerById]);
+  }, [volunteerData?.variables]);
 
   const formik = useFormik({
     initialValues: {
@@ -51,12 +51,12 @@ export default function LoginScreen({navigation}) {
     },
     validationSchema: loginValidationSchema,
     onSubmit: values => {
-      const {email, password} = values;
+      const { email, password } = values;
       setIsLoading(true);
       login(email, password)
         .then(res => {
           getVolunteer({
-            variables: {volunteerId: res.attributes.sub},
+            variables: { volunteerId: res.attributes.sub },
           });
         })
         .catch(err => {
